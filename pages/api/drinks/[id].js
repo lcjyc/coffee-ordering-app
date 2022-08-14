f862a5e9bea293e52350298601c28c74a765f538
Lesley Chen <86543176+lcjyc@users.noreply.github.com>
2022-08-14 16:27:53 +0800

    Update [id].js

--- pages/api/drinks/[id].js
+++ pages/api/drinks/[id].js
@@ -20,22 +20,8 @@ export default async function handler(req, res) {
     }
   }
 
-  if (method === "PUT") {
-    if (!token || token !== process.env.token) {
-      return res.status(401).json("Not authenticated!");
-    }
-    try {
-      const drink = await Drink.findByIdAndUpdate(id, req.body, {
-        new: true,
-      });
-      res.status(201).json(drink);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  }
-
   if (method === "DELETE") {
-    if (!token || token !== process.env.token) {
+    if (!token || token !== process.env.TOKEN) {
       return res.status(401).json("Not authenticated!");
     }
     try {
