66e7afb69a6dcd60dc0b09a2f28b3b2f4963ab94
Lesley Chen <86543176+lcjyc@users.noreply.github.com>
2022-09-05 19:37:39 +0800

    Update OrderDetail.jsx

--- components/OrderDetail.jsx
+++ components/OrderDetail.jsx
@@ -11,7 +11,7 @@ const OrderDetail = ({ total, createOrder }) => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <h1 className={styles.title}>You will pay NT 120 after delivery.</h1>
+        <h1 className={styles.title}>You will pay NT.{total} after delivery.</h1>
         <div className={styles.item}>
           <label className={styles.label}>Your Name</label>
           <input
